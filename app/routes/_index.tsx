0b5d15584bd606187b59b2065c27c668bfc21ad6
simonwallstrom <simon.wallstrom@gmail.com>
2024-01-31 00:20:06 +0100

    Updated font sizes for start page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,10 @@
 export default function Index() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="text-[12vw] font-bold leading-none tracking-tight">Cookbase</h1>
-      <p className="ml-1 font-mono text-[3vw] font-semibold tracking-tight">
+      <h1 className="-mt-4 text-[clamp(72px,13vw,200px)] font-medium leading-none tracking-tight text-white">
+        Cookbase
+      </h1>
+      <p className="ml-1 font-mono text-[clamp(16px,2vw,30px)] tracking-tight">
         Recipe organizer for families
       </p>
     </div>
