a42c43d05fb0d09cf31f1ebb03192ece512febe0
simonwallstrom <simon.wallstrom@gmail.com>
2024-01-31 20:52:47 +0100

    Updated start page font sizes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,16 +3,12 @@ import { Link } from '@remix-run/react'
 export default function Index() {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
-      <h1 className="-mt-10 text-[clamp(72px,13vw,200px)] font-medium leading-tight tracking-tight dark:text-white">
-        Cookbase
-      </h1>
-      <p className="ml-1 font-mono text-[clamp(16px,2vw,30px)] tracking-tight">
-        Recipe organizer for families
-      </p>
+      <h1 className="-mt-10 text-[10vw] font-medium leading-none tracking-tight">Cookbase</h1>
+      <p className="text-[3vw] text-gray-500 lg:ml-1">Recipe organizer for families</p>
       <div className="absolute bottom-4 lg:bottom-8">
         <Link
           to="/recipes"
-          className="px-8 py-2 font-mono font-semibold underline-offset-8 hover:underline lg:text-lg"
+          className="px-8 py-2 font-medium underline-offset-8 hover:underline lg:text-lg"
         >
           Login →
         </Link>
